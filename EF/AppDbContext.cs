2d30d6b35d225eb27fb7b386f6a552900a3f18e6
Devis Lam <devislam@alphareds.com>
2025-05-12 15:23:56 +0800

    update: fix db context access denied

--- EF/AppDbContext.cs
+++ EF/AppDbContext.cs
@@ -27,9 +27,7 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<Usertoken> Usertokens { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;user=root;password=elvinlam0814;database=secondlifeunimas_db", ServerVersion.Parse("8.0.33-mysql"));
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
